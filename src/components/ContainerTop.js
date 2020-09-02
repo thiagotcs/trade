9e4fb9df7f6d6ec3629b5239b3d49cb1a7e12373
thiago carvalho <thiagocarvalho@DESKTOP-F6LHES7.localdomain>
2020-09-02 18:38:22 -0300

    criada a tela de login

---
+++ src/components/ContainerTop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import Image from '../assets/trade.png';
+
+export default function Album() {
+  return (
+    <AppBar position="relative">
+      <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+          <img src={Image} />
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
